9f10233fb55378fbceddf7a6350d3a134f548072
MarioAguilarReal <mario.real@uabc.edu.mx>
2023-07-05 15:13:04 -0700

    first commit

---
+++ posts/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+
+from .models import Post
+
+class PostListView(ListView):
+    template_name = "post/list.html"
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = "post/detail.html"
+    model = Post
+
+class PostCreateView(CreateView):
+    template_name = "post/new.html"
+    model = Post
+    fields = ['title', 'subtitle', 'body']
